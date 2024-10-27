7432bbe28affb1a1bda38e32690df0af6cccbf66
Ellleanor <120443550+Ellleanor@users.noreply.github.com>
2024-10-27 16:53:18 +0800

    Update buddy_system.c

--- lab2/kern/mm/buddy_system.c
+++ lab2/kern/mm/buddy_system.c
@@ -17,12 +17,13 @@ struct buddy
 }buddy;
 
 static size_t up_power_of_2(size_t n){
-    n |= n >> 1;
-    n |= n >> 2;
-    n |= n >> 4;
-    n |= n >> 8;
-    n |= n >> 16;
-    return n+1;
+    int n = 0, tmp = size;
+    while (tmp >>= 1)
+    {
+        n++;
+    }
+    n += 1;
+    return (1 << n);
 };
 
 
