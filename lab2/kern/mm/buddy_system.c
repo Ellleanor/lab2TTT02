c5dc79bd43191fd1fa110ec188495972c56fa781
Ellleanor <120443550+Ellleanor@users.noreply.github.com>
2024-10-27 18:00:38 +0800

    Update buddy_system.c

--- lab2/kern/mm/buddy_system.c
+++ lab2/kern/mm/buddy_system.c
@@ -1,20 +1,20 @@
 #include <pmm.h>
 #include <buddy_system.h>
 
+
 extern free_area_t free_area;
 
 #define free_list (free_area.free_list)
 #define nr_free (free_area.nr_free)
 
-struct buddy
+struct Buddy
 {
-    /* data */
-    size_t size;
-    uintptr_t* longest;
-    size_t manage_page_used;
-    size_t free_size;
-    struct Page* begin_page;
-}buddy;
+    unsigned *longest;
+    struct Page *begin_page;
+    unsigned size;
+
+} buddy;
+
 
 static size_t up_power_of_2(size_t n){
     n |= n >> 1;
@@ -25,84 +25,84 @@ static size_t up_power_of_2(size_t n){
     return n+1;
 }
 
-static void buddy_init(){
+static void buddy_init(void)
+{
     list_init(&free_list);
     nr_free = 0;
 }
 
-static void buddy_init_memmp(struct Page* base,size_t n){
-    
+static void buddy_init_memmap(struct Page *base, size_t n)
+{
+    //base is a virtual page,indicating the beginning of pages
     assert(n > 0);
-
     size_t real_need_size = up_power_of_2(n);
 
-    buddy.begin_page = base;
-
-    struct Page* page = buddy.begin_page;
-
-    for (; page != buddy.begin_page + n; page++) {
-        assert(PageReserved(page));
-        // 清空当前页框的标志和属性信息，并将页框的引用计数设置为0
-        page->flags = page->property = 0;
-        set_page_ref(page, 0);
+    struct Page *p = base;
+    for (; p != base + n; p++)
+    {
+        assert(PageReserved(p));
+        p->flags = p->property = 0;
+        set_page_ref(p, 0);
     }
+    
+    base->property = n; // 从base开始有n个可用页
+    p = base + n; 
+
+    buddy.begin_page = base;
+    buddy.longest = (unsigned *)p;
+
 
     buddy.size = real_need_size;
 
-    buddy.free_size = n;
+    unsigned node_size = 2 * real_need_size;
 
-    buddy.longest = (uintptr_t*)(base + n);         //结尾放二叉树结构
-
-    base->property = n;
-
-    
-    size_t node_size = real_need_size*2;
-
-    for(int i=0;i<2*real_need_size-1;++i){
-        if(IS_POWER_OF_2(i+1)){
+    for (int i = 0; i <2 * real_need_size - 1; ++i)
+    {
+        if (IS_POWER_OF_2(i + 1))
+        {
             node_size /= 2;
         }
         buddy.longest[i] = node_size;
-    }
 
+    }
     nr_free += n;
-    
 }
 
-static struct Page *
-buddy_alloc_pages(size_t n) {
+static struct Page *buddy_alloc_pages(size_t n)
+{
     assert(n > 0);
-    if(n>nr_free || buddy.longest[0]<n){
+    unsigned index = 0;
+    
+    if (n > nr_free || buddy.longest[index] < n)
+    {
         return NULL;
     }
+    
+    unsigned node_size;
+    unsigned offset = 0;
 
-   
-    size_t real_apply_size;
-    size_t offset = 0;
+    size_t real_alloc = n;
 
-    if(!IS_POWER_OF_2(n)){
-        real_apply_size = up_power_of_2(n); 
-    }else{
-        real_apply_size = n;
+    if (!IS_POWER_OF_2(n))
+    {
+        real_alloc = up_power_of_2(n);
     }
-
-
-    size_t index = 0;
-
-
-    int node_size;
-
-    for(node_size = buddy.size ; node_size!= real_apply_size;node_size/2){
-        if(buddy.longest[LEFT_LEAF(index)] >= real_apply_size){
+    
+    for (node_size = buddy.size; node_size != real_alloc; node_size /= 2)
+    {
+        
+        if (buddy.longest[LEFT_LEAF(index)] >= real_alloc)
             index = LEFT_LEAF(index);
-        }else{
+        else{
             index = RIGHT_LEAF(index);
         }
     }
 
+    
+
     buddy.longest[index] = 0;
 
-    offset = (index+1)*node_size - buddy.size;
+    offset = (index + 1) * node_size - buddy.size;
 
     while(index){
         index = PARENT(index);
@@ -111,53 +111,47 @@ buddy_alloc_pages(size_t n) {
 
 
 
-    struct Page *page = NULL;
-    struct Page* base_page = buddy.begin_page + offset;
+    struct Page *base_page = buddy.begin_page + offset;
+    struct Page *page;
 
-    for(page = buddy.begin_page + offset ;page!=buddy.begin_page + real_apply_size;page++)
+    // 将每一个取出的块由空闲态改为保留态
+    for (page = base_page; page != base_page + real_alloc ; page++)
     {
         ClearPageProperty(page);
     }
 
-    nr_free -= real_apply_size;
-
-    base_page->property = real_apply_size;
-
-    buddy.free_size -= real_apply_size;
-
+    base_page->property = real_alloc;  //用n来保存分配的页数，n为2的幂
+    nr_free -= real_alloc;
     return base_page;
-    
 }
 
-
-static void
-buddy_free_pages(struct Page *base, size_t n) {
-    
+static void buddy_free_pages(struct Page *base, size_t n)
+{
     assert(n > 0);
+    unsigned node_size, index = 0;
+    unsigned left_longest, right_longest;
+    
+    int total_page = n;
 
-    if(!IS_POWER_OF_2(n)){
+    if (!IS_POWER_OF_2(n))
+    {
         n = up_power_of_2(n);
     }
 
-    size_t node_size = 1;
-    size_t index = 0;
-    size_t left_longest,right_longest;
-
-    size_t offset = base - buddy.begin_page;
-
-    index = offset + buddy.size - 1;
+    int offset = (base - buddy.begin_page);
+    node_size = 1;
+    index = buddy.size + offset - 1;
 
     while (node_size != n)
     {
-        node_size *=2;
+        node_size *= 2;
         index = PARENT(index);
-        if(index == 0){
+        if (index == 0)
             return;
-        }
     }
-    
-    
+
     buddy.longest[index] = node_size;
+
 
     while(index){
         index = PARENT(index);
@@ -174,67 +168,93 @@ buddy_free_pages(struct Page *base, size_t n) {
         }
 
     }
-
+    
     nr_free+=n;
-
-
-
+    
 }
 
-
 static size_t
-buddy_nr_free_pages(void) {        //返回剩余的空闲的页数
+buddy_nr_free_pages(void) {
     return nr_free;
 }
 
-static void buddy_check(){
 
+static void
+buddy_check(void) {
+    
+    struct Page *p0, *p1, *p2, *p3, *p4;
+    p0 = p1 = p2 = p3 = p4 = NULL;
 
-    size_t total_page = buddy_nr_free_pages();
-    struct Page *p0, *p1, *p2;
-    p0 = p1 = p2 = NULL;
     assert((p0 = alloc_page()) != NULL);
     assert((p1 = alloc_page()) != NULL);
     assert((p2 = alloc_page()) != NULL);
 
     assert(p0 != p1 && p0 != p2 && p1 != p2);
     assert(page_ref(p0) == 0 && page_ref(p1) == 0 && page_ref(p2) == 0);
+    assert(p1==p0+1 && p2 == p1+1);  //页面地址关系
 
-    assert(page2pa(p0) < npage * PGSIZE);       //测试不超出界限
+    assert(page2pa(p0) < npage * PGSIZE);
     assert(page2pa(p1) < npage * PGSIZE);
     assert(page2pa(p2) < npage * PGSIZE);
 
-    assert(p1 == p0 + 1);       //测试连续分页
-
-    buddy_free_pages(p0,1);
-    buddy_free_pages(p1,1);
-    buddy_free_pages(p2,1);
-
-    assert(buddy_nr_free_pages() == total_page );
-
-    struct Page* p4 = NULL;
-    p4 = buddy_alloc_pages(256);
-    assert(buddy_nr_free_pages() == ( total_page -256));
-
-    struct Page* p5 = NULL;
-    p5 = buddy_alloc_pages(256);
-    assert(buddy_nr_free_pages() == ( total_page -2*256));
-
-    buddy_free_pages(p4,256);
-    buddy_free_pages(p5,256);
-
+    free_page(p0);    
+    free_page(p1);
+    free_page(p2);
     
-    assert(buddy_nr_free_pages() == total_page );
+    p1 = alloc_pages(512); //p1应该指向最开始的512个页
+    p2 = alloc_pages(512);
+    p3 = alloc_pages(1024);
+
+    assert(p3 - p2 == p2 - p1);//检查相邻关系
+
+    free_pages(p1, 256);
+    free_pages(p2, 512);
+    free_pages(p1 + 256, 256);
+    free_pages(p3,1024);
+    //检验释放页时，相邻内存的合并
+
+    p0 = alloc_pages(8192);
+
+    assert(p0 == p1); //重新分配，p0也指向最开始的页
+
+    p1 = alloc_pages(128);
+    p2 = alloc_pages(64);
+    
+
+    assert(p1 + 128 == p2);// 检查是否相邻
+
+    p3 = alloc_pages(128);
 
 
+    //检查p3和p1是否重叠
+    assert(p1 + 256 == p3);
+    
+    //释放p1
+    free_pages(p1, 128);
+
+    p4 = alloc_pages(64);
+    assert(p4 + 128 == p2);
+    // 检查p4是否能够使用p1刚刚释放的内存
+
+    free_pages(p3, 128);
+    p3 = alloc_pages(64);
+
+    // 检查p3是否在p2、p4之间
+    assert(p3 == p4 + 64 && p3 == p2 - 64);
+    free_pages(p2, 64);
+    free_pages(p4, 64);
+    free_pages(p3, 64);
+    // 全部释放
+    free_pages(p0, 8192);
+    
 }
 
 const struct pmm_manager buddy_pmm_manager = {
-        .name = "buddy_pmm_manager",
-        .init = buddy_init,
-        .init_memmap = buddy_init_memmp,
-        .alloc_pages = buddy_alloc_pages,
-        .free_pages = buddy_free_pages,
-        .nr_free_pages = buddy_nr_free_pages,
-        .check = buddy_check,
+    .name = "buddy_pmm_manager",
+    .init = buddy_init,
+    .init_memmap = buddy_init_memmap,
+    .alloc_pages = buddy_alloc_pages,
+    .free_pages = buddy_free_pages,
+    .nr_free_pages = buddy_nr_free_pages,
+    .check = buddy_check,
 };
